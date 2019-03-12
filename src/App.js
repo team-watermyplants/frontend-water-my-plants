b923768c42e5e66a3d899e260e844f0387e481f4
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-03-12 16:07:05 -0700

    fixes ProtectRoute component call

--- src/App.js
+++ src/App.js
@@ -1,40 +1,34 @@
-import React, { Component } from "react";
-import { Route, NavLink, withRouter } from "react-router-dom";
-import { connect } from "react-redux";
+import React, { Component } from 'react';
+import { Route, NavLink, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-import ProtectedRoute from "./components/ProtectedRoute";
+import ProtectedRoute from './components/ProtectedRoute';
 
-import PlantForm from "./components/forms/PlantForm";
-import Login from "./components/forms/Login";
-import SignUp from "./components/forms/SignUp";
-import Home from "./components/views/Home";
-import PlantCalendar from "./components/views/PlantCalendar";
-import Plant from "./components/views/Plant";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import AppBar from "material-ui/AppBar";
+import PlantForm from './components/forms/PlantForm';
+import Login from './components/forms/Login';
+import SignUp from './components/forms/SignUp';
+import Home from './components/views/Home';
+import PlantCalendar from './components/views/PlantCalendar';
+import Plant from './components/views/Plant';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
 class App extends Component {
   render() {
     return (
-
       <MuiThemeProvider>
-
         <div className="App">
           <AppBar title="Water My Plants">
             <NavLink to="/">Home</NavLink>
+            <NavLink to="/login">Login</NavLink>
+            <NavLink to="/sign-up">Sign Up</NavLink>
             <NavLink to="/add-plant">Add Plant</NavLink>
           </AppBar>
           <section>
             <Route path="/login" render={props => <Login {...props} />} />
             <Route path="/sign-up" render={props => <SignUp {...props} />} />
             <ProtectedRoute path="/" component={Home} />
-            <ProtectedRoute
-              path="/add-plant"
-              render={props => <PlantForm {...props} />}
-            />
-            <ProtectedRoute
-              path="/plant/:id"
-              render={props => <Plant {...props} />}
-            />
+            <ProtectedRoute path="/add-plant" component={PlantForm} />
+            <ProtectedRoute path="/plant/:id" component={Plant} />
           </section>
         </div>
       </MuiThemeProvider>
