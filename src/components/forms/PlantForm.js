3d064119aa0615ce6285276a916847f45f94193e
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-03-14 01:45:36 -0700

    changes add-plant view to also add plant notification

--- src/components/forms/PlantForm.js
+++ src/components/forms/PlantForm.js
@@ -1,142 +1,138 @@
-import React from "react";
-import { connect } from "react-redux";
-import axios from "axios";
+import React from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
 import DatePicker from 'react-datepicker';
-import "react-datepicker/dist/react-datepicker.css";
+import 'react-datepicker/dist/react-datepicker.css';
 
-import 'moment/locale/it';
+import { addPlant, updatePlant, cancelUpdate } from '../../actions';
 
-import { addPlant, updatePlant, cancelUpdate } from "../../actions";
-
-import ImageResults from "../ImageResults";
+import ImageResults from '../ImageResults';
 
 class PlantForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       plant: {
-        name: "",
-        description: "",
-        location: "",
-        plantURL: ""
+        name: '',
+        description: '',
+        location: '',
+        plantURL: '',
       },
-      searchPlant: "",
-      apiUrl: "https://api.unsplash.com/search/photos",
+      searchPlant: '',
+      apiUrl: 'https://api.unsplash.com/search/photos',
       apiKey:
-        "48117950a0275f34c51b3ddc13c4aa1606f1f38218226bfa626297fe80c98d6b",
+        '48117950a0275f34c51b3ddc13c4aa1606f1f38218226bfa626297fe80c98d6b',
       amount: 1,
       images: [],
-      startDate: new Date()
+      startDate: new Date(),
     };
   }
 
   componentDidMount = () => {
     if (this.props.activePlant) {
       this.setState({
-        plant: this.props.activePlant
+        plant: this.props.activePlant,
       });
     }
-  
   };
 
   searchChangeHandler = e => {
     let val = e.target.value;
     this.setState({ [e.target.name]: val }, () => {
-      val === "" ? this.setState({ images: [] }) :
-        axios
-          .get(
-            `${this.state.apiUrl}/?client_id=${this.state.apiKey}&query=plant+${
-              this.state.searchPlant
-            }&per_page=${this.state.amount}`
-          )
-          .then(res => {
-            this.setState({ images: res.data.results });
-          })
-          .catch(err => console.log(err));
-      }
-    );
+      val === ''
+        ? this.setState({ images: [] })
+        : axios
+            .get(
+              `${this.state.apiUrl}/?client_id=${
+                this.state.apiKey
+              }&query=plant,${this.state.searchPlant}&per_page=${
+                this.state.amount
+              }`
+            )
+            .then(res => {
+              this.setState({ images: res.data.results });
+            })
+            .catch(err => console.log(err));
+    });
   };
 
   changeHandler = e => {
     this.setState({
       plant: {
         ...this.state.plant,
-        [e.target.name]: e.target.value
-      }
+        [e.target.name]: e.target.value,
+      },
     });
   };
 
   selectImage = (e, img) => {
     e.preventDefault();
-    this.setState({ plantURL: img });
-    this.setState({ images: [], searchPlant: "Plant image selected!" });
+    this.setState({
+      images: [],
+      searchPlant: 'Plant image selected!',
+      plantURL: img,
+    });
   };
 
   handleUpdateCancel = () => {
     this.setState({
       plant: {
-        name: "",
-        description: "",
-        location: "",
-        plantURL: ""
+        name: '',
+        description: '',
+        location: '',
+        plantURL: '',
       },
-      searchPlant: "",
-      apiUrl: "https://api.unsplash.com/search/photos",
+      searchPlant: '',
+      apiUrl: 'https://api.unsplash.com/search/photos',
       apiKey:
-        "48117950a0275f34c51b3ddc13c4aa1606f1f38218226bfa626297fe80c98d6b",
+        '48117950a0275f34c51b3ddc13c4aa1606f1f38218226bfa626297fe80c98d6b',
       amount: 1,
-      images: []
+      images: [],
     });
 
     this.props.cancelUpdate();
-    this.props.history.push("/");
+    this.props.history.push('/');
   };
 
   handleSubmit = e => {
     e.preventDefault();
-    const userId = localStorage.getItem("userId");
-    console.log("\nid", userId);
+    const userId = localStorage.getItem('userId');
+    console.log('\nid', userId);
     const newPlant = {
       ...this.state.plant,
-      userId
+      userId,
+      startDate: this.state.startDate,
     };
     console.log(newPlant);
     this.props.activePlant
       ? this.props.updatePlant(this.props.activePlant.id, newPlant).then(() => {
-          this.props.history.push("/");
+          this.props.history.push('/');
         })
       : this.props.addPlant(newPlant).then(() => {
-          this.props.history.push("/");
+          this.props.history.push('/');
         });
     this.setState({
       plant: {
-        name: "",
-        searchPlant: "",
-        location: "",
-        description: "",
-        plantURL: ""
-      }
+        name: '',
+        searchPlant: '',
+        location: '',
+        description: '',
+        plantURL: '',
+      },
     });
   };
 
-  changeHandler = e => {
-      this.setState({
-          ...this.state.plant,
-          [e.target.name]: e.target.value
-      })
-  }
-
   handleDateChange = date => {
-      console.log(date)
-      this.setState({
-          startDate: date
-      })
-  }
+    console.log(date);
+    this.setState({
+      startDate: date,
+    });
+  };
 
   render() {
     return (
       <div>
-        <h1>{this.props.activePlant ? "update plant" : "add plant"}</h1>
+        <h1>{this.props.activePlant ? 'update plant' : 'add plant'}</h1>
         <form onSubmit={this.handleSubmit}>
           <input
             type="text"
@@ -163,15 +159,15 @@ class PlantForm extends React.Component {
           />
           <br />
           <select>
-              <option>Every Day</option>
-              <option>Every Other Day</option>
+            <option>Every Day</option>
+            <option>Every Other Day</option>
           </select>
           <br />
           <DatePicker
             selected={this.state.startDate}
             onChange={this.handleDateChange}
             showTimeSelect
-     />
+          />
           <br />
           <input
             type="text"
@@ -188,7 +184,7 @@ class PlantForm extends React.Component {
           ) : null}
           <br />
           <button onClick={this.handleSubmit}>
-            {this.props.activePlant ? "update plant" : "add plant"}
+            {this.props.activePlant ? 'update plant' : 'add plant'}
           </button>
           {this.props.activePlant ? (
             <button onClick={this.handleUpdateCancel}>cancel</button>
@@ -203,7 +199,7 @@ class PlantForm extends React.Component {
 
 const mapStateToProps = state => ({
   addingPlant: state.addingPlant,
-  activePlant: state.updateReducer.activePlant
+  activePlant: state.updateReducer.activePlant,
 });
 
 export default connect(
