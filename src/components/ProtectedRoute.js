d85a78df9cb5a7d38ffd9cc27ee78a2c964374a2
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-03-18 00:51:43 -0700

    implements github login; hacky addition to ProtectedRoute

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 const ProtectedRoute = ({ component: Component, ...rest }) => {
+  //TODO: Clean up this hackie social login implementation
+  const search = window.location.search.split('=')
+  console.log('\n🦄',search)
+  const token = search[1]
+  const userIdQuery = search[2]
+  if (token && userIdQuery) {
+    localStorage.setItem('token', token)
+    localStorage.setItem('userId', userIdQuery)
+  }
   return (
     <Route
       {...rest}
